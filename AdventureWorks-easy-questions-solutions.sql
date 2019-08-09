83f18d9909a83a74b53ca48457f434832dd003e2
beatAffection <53842484+beatAffection@users.noreply.github.com>
2019-08-09 04:29:38 -0400

    Update AdventureWorks-easy-questions-solutions.sql

--- AdventureWorks-easy-questions-solutions.sql
+++ AdventureWorks-easy-questions-solutions.sql
@@ -8,12 +8,27 @@ WHERE CompanyName = 'Bike World'
 
 --2.
 /*Show the CompanyName for all customers with an address in City 'Dallas'.*/
+SELECT CompanyName
+FROM Customer JOIN CustomerAddress ON (Customer.CustomerID=CustomerAddress.CustomerID)
+              JOIN Address         ON (CustomerAddress.AddressID=Address.AddressID)
+Where City = 'Dallas'
 
 --3.
 /*How many items with ListPrice more than $1000 have been sold?*/
+SELECT Count(ProductID)
+FROM Product
+WHERE ListPrice > 1000
 
 --4.
 /*Give the CompanyName of those customers with orders over $100000. Include the subtotal plus tax plus freight.*/
+SELECT CompanyName
+FROM Customer JOIN SalesOrderHeader ON (Customer.CustomerID=SalesOrderHeader.CustomerID)
+WHERE (SubTotal+TaxAmt+Freight) > 100000
 
 --5.
 /*Find the number of left racing socks ('Racing Socks, L') ordered by CompanyName 'Riding Cycles'*/
+SELECT Count(Name)
+FROM Product JOIN SalesOrderDetail ON (Product.ProductID=SalesOrderDetail.ProductID)
+             JOIN SalesOrderHeader ON (SalesOrderDetail.SalesOrderID=SalesOrderHeader.SalesOrderID)
+             JOIN Customer         ON (SalesOrderHeader.CustomerID=Customer.CustomerID)
+WHERE Name = 'Racing Socks, L' AND CompanyName = 'Riding Cycles'
