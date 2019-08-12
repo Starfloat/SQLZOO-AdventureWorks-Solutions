d80722739fc2b81e1622f17d927fa781dc950fa0
beatAffection <53842484+beatAffection@users.noreply.github.com>
2019-08-12 17:57:34 -0400

    Create AdventureWorks-hard-questions-solutions.sql

---
+++ AdventureWorks-hard-questions-solutions.sql
@@ -0,0 +1,25 @@
+/*https://sqlzoo.net/wiki/AdventureWorks_hard_questions*/
+
+--11.
+/*For every customer with a 'Main Office' in Dallas show AddressLine1 of the 'Main Office' and AddressLine1 of the 'Shipping' address - if there is no shipping address leave it blank. 
+Use one row per customer.
+*/
+
+--12.
+/*For each order show the SalesOrderID and SubTotal calculated three ways: 
+A) From the SalesOrderHeader 
+B) Sum of OrderQty*UnitPrice 
+C) Sum of OrderQty*ListPrice
+*/
+
+--13.
+/*Show the best selling item by value.
+*/
+
+--14.
+/*Show how many orders are in the following ranges (in $):
+*/
+
+--15.
+/*Identify the three most important cities. Show the break down of top level product category against city.
+*/
